62c0565d56bd6f7dff717e63e99d726cd77d6e63
Kim Jeong-won <threedalpeng@gmail.com>
2023-11-30 16:55:48 +0900

    fix sveltekit config

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : 'music-lab'
+			base: process.argv.includes('dev') ? '' : '/music-lab'
 		}
 	}
 };
